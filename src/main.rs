6b1abe17f2b98fb723b4e135fd390b73bf9766bc
Robledo Pazotto <robledo@gmail.com>
2025-03-06 06:44:43 -0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,9 @@ fn main() {
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                println!(
-                    "Received {} bytes from {}: {}",
-                    size,
-                    source,
-                    std::str::from_utf8(&buf).unwrap()
-                );
-                let response = [];
+                let message = std::str::from_utf8(&buf).unwrap();
+                println!("Received {} bytes from {}: {}", size, source, message);
+                let response: [u8; 12] = [0x04, 0xd2, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
