883d4c53e59c764c82b02713063b38262b3c9bde
Robledo Pazotto <robledo@gmail.com>
2025-03-07 12:48:54 -0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -29,87 +29,80 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((_, source)) => {
                 let request: DnsPacket = DnsPacket::parse(buf.to_vec().into_boxed_slice());
-                request_queue.push((request.header.id, source, request.clone()));
 
+                println!("Received RESPONSE: {:?}", request);
+
+                // It's a response
                 if request.header.qr {
-                    if request.answers.len() > 0 {
-                        println!("Received response from {:?}: {:?}", source, request.clone());
+                    println!("Received response from {:?}: {:?}", source, request.clone());
 
-                        let mut response = request.clone();
-                        // response.header.qr = true;
+                    let mut response = request.clone();
 
-                        let index = request_queue
+                    let request_index = request_queue
+                        .iter()
+                        .position(|x| x.0 == response.header.id)
+                        .unwrap();
+
+                    let (_, client_address, original_request) =
+                        request_queue.get(request_index).unwrap();
+
+                    if original_request.header.qdcount > 1 {
+                        let response_index = response_queue
                             .iter()
-                            .position(|x| x.0 == response.header.id)
-                            .unwrap();
+                            .position(|x| x.0 == original_request.header.id);
 
-                        let (_, client_address, original_request) =
-                            request_queue.get(index).unwrap();
+                        if response_index.is_some() {
+                            let (_, address, existing_response) =
+                                response_queue.get(response_index.unwrap()).unwrap();
+                            let mut existing_response = existing_response.clone();
+                            existing_response.header.qdcount += 1;
+                            existing_response.header.ancount += 1;
 
-                        if original_request.header.qdcount > 1 {
-                            println!(
-                                "Looking for response with id {}, response_queue: {:?}",
-                                response.header.id, response_queue
-                            );
-                            let response_index = response_queue
-                                .iter()
-                                .position(|x| x.0 == original_request.header.id);
+                            existing_response
+                                .questions
+                                .extend(response.questions.clone());
+                            existing_response.answers.extend(response.answers.clone());
 
-                            if response_index.is_some() {
+                            if existing_response.header.ancount == original_request.header.qdcount {
                                 println!(
-                                    "Index found for request id {}",
-                                    original_request.header.id
+                                    "Sending COMBINED response to {:?}: {:?}",
+                                    address, existing_response
                                 );
+                                udp_socket
+                                    .send_to(&existing_response.to_bytes(), address)
+                                    .expect("Failed to send response");
 
-                                let (_, address, existing_response) =
-                                    response_queue.get(response_index.unwrap()).unwrap();
-                                let mut existing_response = existing_response.clone();
-                                existing_response.header.qdcount += 1;
-                                existing_response.header.ancount += 1;
-
-                                existing_response
-                                    .questions
-                                    .extend(response.questions.clone());
-                                existing_response.answers.extend(response.answers.clone());
-
-                                println!("existing_response: {:?}", existing_response);
-
-                                if existing_response.header.ancount
-                                    == original_request.header.qdcount
-                                {
-                                    println!(
-                                        "Sending COMBINED response to {:?}: {:?}",
-                                        address, existing_response
-                                    );
-                                    udp_socket
-                                        .send_to(&existing_response.to_bytes(), address)
-                                        .expect("Failed to send response");
-                                } else {
-                                    println!(
-                                        "existing ancount = {}, original ancount = {}",
-                                        existing_response.header.ancount,
-                                        original_request.header.ancount
-                                    );
-                                }
-                            } else {
-                                println!("Inserting response with id {}", original_request.header.id);
-
-                                response_queue.push((
-                                    original_request.header.id,
-                                    *client_address,
-                                    response.clone(),
-                                ));
-
-                                println!("response_queue: {:?}", response_queue);
+                                response_queue.remove(response_index.unwrap());
+                                request_queue.remove(request_index);
                             }
                         } else {
-                            println!("Sending response to {:?}: {:?}", client_address, response);
-                            udp_socket
-                                .send_to(&response.to_bytes(), client_address)
-                                .expect("Failed to send response");
+                            response_queue.push((
+                                original_request.header.id,
+                                *client_address,
+                                response.clone(),
+                            ));
                         }
+                    } else {
+                        println!("Sending response to {:?}: {:?}", client_address, response);
+                        println!("response_queue: {:?}", response_queue);
+                        println!("request_queue: {:?}", request_queue);
+
+                        udp_socket
+                            .send_to(&response.to_bytes(), client_address)
+                            .expect("Failed to send response");
+
+                        let response_index = response_queue
+                            .iter()
+                            .position(|x| x.0 == response.header.id);
+                        if response_index.is_some() {
+                            response_queue.remove(response_index.unwrap());
+                        }
+
+                        request_queue.remove(request_index);
                     }
                 } else {
+                    request_queue.push((request.header.id, source, request.clone()));
+                    // It's a query
                     println!("Received query from {:?}: {:?}", source, request.clone());
 
                     if request.header.qdcount > 1 {
@@ -126,6 +119,8 @@ fn main() {
                                 resolver_address.clone(),
                                 forwarded_request.clone()
                             );
+
+                            forwarded_request.header.qr = false;
 
                             udp_socket
                                 .send_to(&forwarded_request.to_bytes(), resolver_address.clone())
